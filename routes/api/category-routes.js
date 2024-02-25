eb76539a60d573a1cc6c26224910e6177b99cde7
AliahG97 <143744575+AliahG97@users.noreply.github.com>
2024-02-24 21:31:13 -0500

    edited all the routes, but updates and delete stilld ont work in insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 // find all categories
 // be sure to include its associated Products
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const categories = await Category.findAll({ include: Product });
     res.json(categories);
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   try {
     const category = await Category.findByPk(req.params.id, { include: Product });
     if (!category) {
-      res.status(404).json({ message: 'Category not found'});
+      res.status(404).json({ message: 'Category not found' });
     } else {
       res.json(category);
     }
@@ -30,16 +30,44 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const category = await Category.beforeBulkCreate(req.body);//create.(req.body)
+    res(201).json(category);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+    } else {
+      await category.update(req.body);
+      res.status(200).json({ message: 'Category updated succefully' });
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+      res.status(404)({ message: 'Category not found' });
+    } else {
+      await category.destroy();
+      res.status(200).json({ message: 'Category deleted successfully' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
